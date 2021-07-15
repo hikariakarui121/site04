e6b8e66b10da85f89c3daf4111dd284234f562c5
hikariakarui121 <80142147+hikariakarui121@users.noreply.github.com>
2021-07-16 05:15:08 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -14,21 +14,21 @@
 //   },
 // });
 
-// var slider1 = new Swiper('.slider1', {
-//       slidesPerView: 1,
-//       spaceBetween: 30,
-//       effect:'fade',
-//       loop:true,
-//       pagination: {
-//         el: '.swiper-pagination',
-//         clickable: true,
-//       },
+var slider1 = new Swiper('.slider1', {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      effect:'fade',
+      loop:true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
       
-//       autoplay: {
-//         delay: 3000,
-//         disableOnInteraction: true,
-//       }
-//     });
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction: true,
+      }
+    });
 
 // ふたつ目のスライドショー
 var slider2 = new Swiper('.slider2', {
